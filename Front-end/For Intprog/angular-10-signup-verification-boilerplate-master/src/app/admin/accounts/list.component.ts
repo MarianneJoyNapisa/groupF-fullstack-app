d502c66564ffe0546e70a24d1228569fda17f599
Jomari Marson <jomari.marson@gmail.com>
2025-05-03 20:46:47 +0800

    Front-end and Fake Back-end

---
+++ Front-end/For Intprog/angular-10-signup-verification-boilerplate-master/src/app/admin/accounts/list.component.ts
@@ -0,0 +1,47 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+
+import { AccountService } from '@app/_services';
+import { Account } from '@app/_models';
+
+@Component({ templateUrl: 'list.component.html' })
+export class ListComponent implements OnInit {
+    accounts: any[];
+
+    constructor(
+        private accountService: AccountService,
+        private router: Router,
+        private route: ActivatedRoute
+    ) {}
+
+    // In list.component.ts
+    ngOnInit() {
+        this.accountService.getAll()
+            .pipe(first())
+            .subscribe(accounts => {
+                // Remove any hardcoded status assignment
+                this.accounts = accounts;
+            });
+    }
+    
+    loadAccounts() {
+        this.accountService.getAll()
+            .pipe(first())
+            .subscribe(accounts => {
+                this.accounts = accounts;
+            });
+    }
+    
+    /*
+    deleteAccount(id: string) {
+        const account = this.accounts.find(x => x.id === id);
+        account.isDeleting = true;
+        this.accountService.delete(id)
+            .pipe(first())
+            .subscribe(() => {
+                this.accounts = this.accounts.filter(x => x.id !== id) 
+            });
+    }
+    */
+}
